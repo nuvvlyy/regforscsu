f2e48b44a265d773321499ff543d656868260247
nuvvlyy <nutnutnut_1998@hotmail.com>
2018-08-27 11:35:59 +0700

    testtest5

--- resources/views/temptest.blade.php
+++ resources/views/temptest.blade.php
@@ -3,6 +3,11 @@
 <head >
     <title>#sciweek2018 </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <style>
+        @page {
+            size: 59mm 125cm;
+        }
+    </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script>
